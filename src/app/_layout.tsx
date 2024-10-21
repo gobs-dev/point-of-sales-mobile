5a85d3db3d69ebfbeb988932828eaf3b4b2d88c8
Muhamad Taopik Gibran <49028961+mgibrann@users.noreply.github.com>
2024-10-21 20:59:08 +0700

    adjust layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,14 +4,14 @@ import '../../global.css';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { ThemeProvider } from '@react-navigation/native';
 import { SplashScreen, Stack } from 'expo-router';
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 import { APIProvider } from '@/api';
-import { hydrateAuth, loadSelectedTheme } from '@/core';
+import {  checkAuthStatus, loadSelectedTheme, useAuth } from '@/core';
 import { useThemeConfig } from '@/core/use-theme-config';
 
 export { ErrorBoundary } from 'expo-router';
@@ -20,14 +20,47 @@ export const unstable_settings = {
   initialRouteName: '(app)',
 };
 
-hydrateAuth();
 loadSelectedTheme();
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+function LoadingScreen() {
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+}
+function AuthInitializer() {
+  const status = useAuth.use.status();
+
+  useEffect(() => {
+    const initialize = async () => {
+      console.log('checkAuthStatus')
+      await checkAuthStatus();
+      await SplashScreen.hideAsync();
+    };
+
+    initialize();
+  }, []);
+
+  if (status === 'loading' || status === 'idle') {
+    return <LoadingScreen />; 
+   }
+
+  return null;
+}
+
+
 export default function RootLayout() {
   return (
     <Providers>
+            <AuthInitializer />
+
       <Stack>
         <Stack.Screen name="(app)" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
